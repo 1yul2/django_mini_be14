1ed50d4bf7eb76f28fec16d3998142c218878c24
choiguen <daindj0@gmail.com>
2025-11-26 02:34:16 +0900

    ✨ feat: 세부내용 검색 기능 추가.

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import Transaction
 
+
 @admin.register(Transaction)
-class TransactionsAdmin(admin.ModelAdmin):
-     list_display = ['amount', 'created_at','balance','is_deposit']
-     list_filter = ['created_at', 'is_deposit']
-     search_fields = ['description']
+class TransactionAdmin(admin.ModelAdmin):
+     list_display = ["account__account_name", "is_deposit", "transacted_at"]
+     list_filter = ["account_id__account_name", "is_deposit", "transacted_at"]
+     search_fields = ["description"]
      ordering = ['-created_at']
