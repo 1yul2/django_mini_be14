8151ad19578c0eb7b5f201c7a634f241e36befa7
choiguen <daindj0@gmail.com>
2025-11-26 02:38:00 +0900

    ✨ feat: get_description추가 및 코드 수정.

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,17 +1,55 @@
 from rest_framework import serializers
 from .models import Transaction
 
-class TransactionsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = '__all__'
-
 class TransactionListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Transaction
-        fields = ['id','is_deposit','amount','description','transacted_at']
+    description = serializers.SerializerMethodField()
 
-class TransactionCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
-        fields = ['amount','description','balance','is_deposit','transaction_type','transacted_at']
+        fields = ["id", "is_deposit", "amount", "description", "transacted_at"]
+
+    def get_description(self, obj):
+        value = obj.description or ""
+        if len(value) > 10:
+            value = value[:10] + "..."
+            return value
+        return value
+
+
+class TransactionDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = "__all__"
+
+
+class TransactionCreateSerializer(serializers.ModelSerializer): 
+    class Meta:
+        model = Transaction
+        fields = [
+            "amount",
+            "balance",
+            "description",
+            "is_deposit",
+            "transaction_type",
+            "transacted_at"
+            ]
+        
+
+
+class TransactionUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = [
+            "amount",
+            "balance",
+            "description",
+            "is_deposit",
+            "transaction_type",
+            "transacted_at"
+            ]
+
+
+class TransactionDeleteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = "__all__"
