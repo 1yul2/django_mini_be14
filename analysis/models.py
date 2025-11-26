de16e98197f2adfe762f6743ef9a33231c608187
choiguen <daindj0@gmail.com>
2025-11-26 16:52:08 +0900

    ✨ feat: 소비 분석.

--- analysis/models.py
+++ analysis/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+class Analysis(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_income = models.BooleanField("분석 대상")
+    periiod = models.CharField("분석 기간", max_length=255)
+    end_date = models.DateTimeField()
+    start_date = models.DateTimeField()
+    description = models.TextField()
+    result_image = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
