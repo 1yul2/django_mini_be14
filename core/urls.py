a69ed9b3a5d9ac5cf332da238775f236e84555b6
ji-min0 <k.jimin2002@gmail.com>
2025-11-27 15:03:18 +0900

    fix: swagger api 문서화

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -36,3 +38,5 @@ urlpatterns = [
     re_path(r"^swagger/$", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui"),
     path("api/accounts/", include("apps.accounts.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
