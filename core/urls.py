1853b95a89af76845fb07f17f0d821ed4631bc43
hochang0819 <strongandrew@naver.com>
2025-11-27 18:41:02 +0900

    feat: analysis migration, 모델 생성.

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,31 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API 문서",
+        default_version="v1",
+        description="가계부 서비스 API 문서입니다.",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("api/accounts/", include("apps.accounts.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
