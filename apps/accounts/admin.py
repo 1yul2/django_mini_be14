df0f8933208a98fc0084ea26b04f9761759042d9
ji-min0 <k.jimin2002@gmail.com>
2025-12-01 14:08:12 +0900

    ✨ feat: accounts admin 내용 추가

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,5 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Accounts, BankCodes
 
-# Register your models here.
+admin.site.register(Accounts)
+admin.site.register(BankCodes)
